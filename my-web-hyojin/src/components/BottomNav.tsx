190dd55a599f2ee25aba1a5bd38901f34ea5fca8
Bacococc <pphj7353@naver.com>
2025-11-04 15:18:23 +0900

    ✨ : About 페이지 구현

--- my-web-hyojin/src/components/BottomNav.tsx
+++ my-web-hyojin/src/components/BottomNav.tsx
@@ -9,7 +9,7 @@ export default async function BottomNav() {
         className="container mx-auto px-10" role="navigation" aria-label="Bottom navigation"
       >
         <div className="flex items-center justify-between h-16">
-          <Link href="/about-me" className="hover:text-gray-100 focus:ring-2 focus:ring-gray-400 focus:outline-none focus:ring-offset-2 focus:ring-offset-black rounded-sm p-2">
+          <Link href="/about-hyojin" className="hover:text-gray-100 focus:ring-2 focus:ring-gray-400 focus:outline-none focus:ring-offset-2 focus:ring-offset-black rounded-sm p-2">
             About me
           </Link>
           <Link href="/project" className="hover:text-gray-100 focus:ring-2 focus:ring-gray-400 focus:outline-none focus:ring-offset-2 focus:ring-offset-black rounded-sm p-2">
