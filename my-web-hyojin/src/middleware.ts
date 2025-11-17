3c309949ff0dc9041f5633a0ff46c2064147f495
Bacococc <pphj7353@naver.com>
2025-11-17 17:59:56 +0900

    📝 : 주석 추가

--- my-web-hyojin/src/middleware.ts
+++ my-web-hyojin/src/middleware.ts
@@ -11,10 +11,12 @@ export function middleware(request: NextRequest) {
 
   const locales = ['ko', 'en', 'ja'];
 
+  // url에 언어 정보가 빠진 경우
   const pathnameIsMissingLocale = locales.every(
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   );
 
+  // /admin 으로 시작되는 url일 경우 쿠키에 어드민 토큰이 없으면 로그인 페이지로 이동
   if (pathname.startsWith('/admin') && pathname !== '/admin/login') {
     const token = request.cookies.get('admin_token')?.value;
     if (!token || token !== process.env.ADMIN_TOKEN) {
@@ -24,6 +26,7 @@ export function middleware(request: NextRequest) {
     }
   }
 
+  // url이 pathnameIsMissingLocale일 경우 쿠키에서 가져옴 -> 없을 경우 ja 를 기본으로 사용
   if (pathnameIsMissingLocale) {
     if (!pathname.startsWith('/admin')) {
       const lang = request.cookies.get('lang')?.value || 'ja';
