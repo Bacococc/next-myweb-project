282e492f6962d4270acade0e6778a036e77fa6e7
Bacococc <pphj7353@naver.com>
2025-11-17 18:52:28 +0900

    🐛 : params 타입 수정

--- my-web-hyojin/src/app/api/admin/projects/update/[id]/route.ts
+++ my-web-hyojin/src/app/api/admin/projects/update/[id]/route.ts
@@ -2,18 +2,26 @@ import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/firebaseConfig";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }  // Promise 포함 타입
+) {
+  const { id } = await params; // Promise 해제
+
   try {
-    const docRef = doc(db, "projects", params.id);
+    const docRef = doc(db, "projects", id);
     const docSnap = await getDoc(docRef);
+
     if (!docSnap.exists()) {
       return NextResponse.json({ error: "프로젝트를 찾을 수 없습니다." }, { status: 404 });
     }
+
     const data = docSnap.data();
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
-    if(error instanceof Error)
-    return NextResponse.json({ error: error.message ?? "서버 에러" }, { status: 500 });
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message ?? "서버 에러" }, { status: 500 });
+    }
   }
 }
 
