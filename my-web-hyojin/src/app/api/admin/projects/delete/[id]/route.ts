5c7d52b64b984278374775624b0f436f9b5a7b7e
Bacococc <pphj7353@naver.com>
2025-11-17 18:44:32 +0900

    🐛 : params를 promise 타입으로 수정

--- my-web-hyojin/src/app/api/admin/projects/delete/[id]/route.ts
+++ my-web-hyojin/src/app/api/admin/projects/delete/[id]/route.ts
@@ -1,19 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/firebaseConfig";
 import { doc, deleteDoc } from "firebase/firestore";
-import { FirebaseError } from "firebase/app";
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await params;
+
   try {
-    const docRef = doc(db, "projects", params.id);
+    const docRef = doc(db, "projects", id);
     await deleteDoc(docRef);
     return NextResponse.json({ message: "삭제 성공" });
   } catch (err) {
-    if(err instanceof FirebaseError){
-      return NextResponse.json({ error: err.message ?? err, message: "삭제 실패" }, { status: 500 });
-    }
+    if (err instanceof Error)
+    return NextResponse.json({ error: err.message ?? err, message: "삭제 실패" }, { status: 500 });
   }
 }
