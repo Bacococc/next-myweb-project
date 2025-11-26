be26f52e1cadda8be30ca39c4d66248b3e54571b
Bacococc <pphj7353@naver.com>
2025-11-26 23:15:44 +0900

    🐛 : lint 요구사항에 맞춰 오류 부분 수정(대부분 타입 오류 수정)

--- my-web-hyojin/src/app/[lang]/layout.tsx
+++ my-web-hyojin/src/app/[lang]/layout.tsx
@@ -5,21 +5,25 @@ import { LangProvider } from '@/i18n/langContext';
 import Header from '../../components/Header';
 import CursorWithStars from '@/app/[lang]/components/CursorWithStars';
 import '@/styles/globals.css';
+import type { Dictionary } from '@/i18n/langContext';
 
 interface Props {
   children: ReactNode;
-  params: { lang: string };
+  // 여기서 Promise 로 변경
+  params: Promise<{ lang: string }>;
 }
 
 export default async function Layout({ children, params }: Props) {
-  const lang = parseLang(params.lang);
-  const dict = await getDictionary(lang); // 언어별 번역 데이터 불러오기
+  // 먼저 params 를 await 해서 lang 꺼내기
+  const { lang } = await params;
+
+  const parsedLang = parseLang(lang);
+  const dict: Dictionary = await getDictionary(parsedLang);
 
   return (
-    <html lang={lang}>
+    <html lang={parsedLang}>
       <body>
-        {/* LangProvider로 언어 상태를 전역적으로 공유 */}
-        <LangProvider lang={lang} dict={dict}>
+        <LangProvider lang={parsedLang} dict={dict}>
           <CursorWithStars />
           <Header />
           <main className="pt-16">{children}</main>
